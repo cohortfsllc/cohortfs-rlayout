9c211e6362cd946b0471a770f30b5685af5bd98d
Fred Isaman <iisaman@netapp.com>
2010-11-12 03:48:32 -0500

    pnfs-submit: Have LAYOUTGETS wait when lo->plh_block_lgets is set
    Preparing for changes to come, change blocking mechanism of LAYOUTGET.
    
    Signed-off-by: Fred Isaman <iisaman@netapp.com>
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>

--- fs/nfs/inode.c
+++ fs/nfs/inode.c
@@ -1449,7 +1449,8 @@ static inline void nfs4_init_once(struct nfs_inode *nfsi)
 	nfsi->delegation = NULL;
 	nfsi->delegation_state = 0;
 	init_rwsem(&nfsi->rwsem);
-	rpc_init_wait_queue(&nfsi->lo_rpcwaitq, "pNFS Layout");
+	rpc_init_wait_queue(&nfsi->lo_rpcwaitq, "pNFS Layoutreturn");
+	rpc_init_wait_queue(&nfsi->lo_rpcwaitq_stateid, "pNFS Layoutstateid");
 	nfsi->layout = NULL;
 #endif
 }

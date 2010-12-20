dongsupark/linux

SQUASHME: pnfs: export get_lseg and put_lseg
Signed-off-by: Fred Isaman <iisaman@netapp.com>

fs/nfs/pnfs.c
@@ -471,6 +471,12 @@ put_lseg(struct pnfs_layout_segment *lseg)
 }
 EXPORT_SYMBOL(put_lseg);
 
+void get_lseg(struct pnfs_layout_segment *lseg)
+{
+	kref_get(&lseg->kref);
+}
+EXPORT_SYMBOL(get_lseg);
+
 static inline u64
 end_offset(u64 start, u64 len)
 {

cohortfsllc/cohortfs-rlayout

fs/nfs/pnfs.c
@@ -366,6 +366,13 @@ put_lseg(struct pnfs_layout_segment *lseg)
 }
 EXPORT_SYMBOL_GPL(put_lseg);
 
+void get_lseg(struct pnfs_layout_segment *lseg)
+{
+	atomic_inc(&lseg->pls_refcount);
+	smp_mb__after_atomic_inc();
+}
+EXPORT_SYMBOL_GPL(get_lseg);
+
 static inline u64
 end_offset(u64 start, u64 len)
 {